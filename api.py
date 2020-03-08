40b2d388f0c316171cba874e3954833fa2f94b89
Ronak Roy <ronak.r123@gmail.com>
2020-03-08 00:25:17 -0500

    Added playlist and saved album fetching, made rudimentary CLI

--- api.py
+++ api.py
@@ -26,18 +26,15 @@ class SpotifyClient(object):
         self.sp = spotipy.Spotify(auth=token)
         self.sp.max_get_retries = 5
 
-    def get_saved_tracks(self):
-        saved_tracks = []
-
+    def _get(self, batch_size, spotipy_get_function, process):
         offset = 0
-        batch_size = 50
-
         total = None
 
         while True:
             if total == None or offset <= total:
-                results = self.sp.current_user_saved_tracks(limit=batch_size, offset=offset)
-                saved_tracks += [Track(result['track']) for result in results['items']]
+                results = spotipy_get_function(limit=batch_size, offset=offset)
+                for result_item in results['items']:
+                    yield process(result_item) 
                 offset += batch_size
             else:
                 break
@@ -45,18 +42,89 @@ class SpotifyClient(object):
             if total == None:
                 total = results['total']
 
-        return saved_tracks
+    def get_playlists(self):
+        playlists = []
+        for playlist in self._get(50, self.sp.current_user_playlists, lambda result_item : Playlist(result_item)):
+            playlists.append(playlist)
+
+        return playlists
+
+    def get_saved_albums(self):
+        albums = []
+        for album in self._get(50, self.sp.current_user_saved_albums, lambda result_item : Album(result_item['album'])):
+            albums.append(album)
+
+        return albums 
+
+    def get_tracks(self, saved=True, albums=[], playlists=[]):
+        tracks = set()
+
+        if saved:
+            for track in self.get_saved_tracks():
+                tracks.add(track)
+
+        for album in albums:
+            for track in self.get_album_tracks(album):
+                tracks.add(track)
+
+        for playlist in playlists:
+            for track in self.get_playlist_tracks(playlist):
+                tracks.add(track)
+                
+        return tracks
+    
+    def get_saved_tracks(self):
+        return self._get(50, self.sp.current_user_saved_tracks, lambda result_item : Track(result_item['track']))
+
+    def get_album_tracks(self, album):
+        return self._get(50, lambda limit, offset : self.sp.album_tracks(album.id, limit=limit, offset=offset), lambda result_item : Track(result_item))
+
+    def get_playlist_tracks(self, playlist):
+        return self._get(50, lambda limit, offset : self.sp.playlist_tracks(playlist.id, limit=limit, offset=offset), lambda result_item : Track(result_item['track']))
 
     def get_features(self, track):
-        results = self.sp.audio_features(track.id)
+        return Features(self.sp.audio_features(track.id)[0])
 
-        return Features(results[0])
+class Artist(object):
+    def __init__(self, spotipy_artist):
+        self.name = spotipy_artist['name']
+        self.id = spotipy_artist['id']
+    
+    def __str__(self):
+        return self.name
+
+class Album(object):
+    def __init__(self, spotipy_album):
+        self.name = spotipy_album['name']
+        self.artists = [Artist(spotipy_artist) for spotipy_artist in spotipy_album['artists']]
+        self.id = spotipy_album['id']
+    
+    def __str__(self):
+        return "{} by {}".format(self.name, utils.get_english_list(self.artists))
+
+class Playlist(object):
+    def __init__(self, spotipy_playlist):
+        self.name = spotipy_playlist['name']
+        self.id = spotipy_playlist['id']
+        self.collab = spotipy_playlist
+
+    def __str__(self):
+        return self.name
 
 class Track(object):
     def __init__(self, spotipy_track):
         self.name = spotipy_track['name']
-        self.artists = [artist['name'] for artist in spotipy_track['artists']]
+        self.artists = [Artist(spotipy_artist) for spotipy_artist in spotipy_track['artists']]
         self.id = spotipy_track['id']
+    
+    def __hash__(self):
+        return hash(self.id)
+    
+    def __eq__(self, other):
+        return self.id == other.id
+
+    def __str__(self):
+        return "{} by {}".format(self.name, utils.get_english_list(self.artists))
 
 class Features(object):
     def __init__(self, spotipy_features):
--- spotidata.py
+++ spotidata.py
@@ -15,19 +15,49 @@ os.environ['SPOTIPY_CLIENT_ID'] = config['client_id']
 os.environ['SPOTIPY_CLIENT_SECRET'] = config['client_secret']
 os.environ['SPOTIPY_REDIRECT_URI'] = config['redirect_uri']
 
-username = config['username']
-scope = 'user-library-read playlist-read-private'
+scope = 'user-library-read playlist-read-private playlist-read-collaborative'
+
+print("="*25)
+print("{:^25s}".format("spotidata"))
+print("="*25)
+
+print("Welcome to spotidata! Let's learn some more about your music taste.")
+print()
+print("Before we get started, you'll need to grant me access to your Spotify account.")
+
+username = input("Enter your Spotify username to get connected (or just hit enter to use the one in config.yml): ")
+if username == "":
+    username = config['username']
 
 with api.login(username, scope) as sp:
+    print()
+    print("Now that I have access to your deepest, darkest music secrets, let's begin. First, you'll need to choose what music you want me to analyze.")
+    use_saved_tracks = utils.input_boolean("Do you want me to analyze your saved songs?")
+    
+    print()
+    print("Cool. Now let's look at your saved albums. I've taken initiative and listed them out here:")
+    queried_albums = utils.input_sublist("Which albums would you like to include in the analysis?", sp.get_saved_albums())
+
+    print()
+    print("One last thing: your playlists.")
+    queried_playlists = utils.input_sublist("Which playlists would you like to include in the analysis?", sp.get_playlists())
+
+    print()
+    print()
+    print("Alright, that's all I need. I'm going to give you everything you could possibly want to know about soungs from:")
+    if use_saved_tracks:
+        print("  - your Saved Music library")
+    if len(queried_albums) != 0:
+        print("  - the following albums: {}".format(utils.get_english_list(queried_albums)))
+    if len(queried_playlists) != 0:
+        print("  - the following playlists: {}".format(utils.get_english_list(queried_playlists)))
+
+    print("="*25)
+
     delim = "---"
 
-    saved_tracks = sp.get_saved_tracks()
-
-    print("Saved tracks")
-    print("="*12)
     print("Song Title{d}Artists{d}Energy{d}Danceability{d}Key{d}Loudness{d}Mode{d}Speechiness{d}Acousticness{d}Instrumentalness{d}Liveness{d}Valence{d}Tempo".format(d=delim))
-
-    for track in saved_tracks:
+    for track in sp.get_tracks(use_saved_tracks, queried_albums, queried_playlists):
         features = sp.get_features(track)
         print("{name}{d}{artists}{d}{features}".format(
             d = delim,
@@ -38,5 +68,3 @@ with api.login(username, scope) as sp:
                 delim
             )
         ))
-
-    print("You have saved a total of {} tracks.".format(len(saved_tracks)))
